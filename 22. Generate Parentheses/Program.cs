28a92bc12e44d5b19f9c8b5635d7b32589652f80
Jason Wu <jsw7524@gmail.com>
2017-11-11 21:16:44 +0800

    Add project files.

---
+++ 22. Generate Parentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Generate_Parentheses
+{
+    public class Solution
+    {
+
+        List<string> Parenthesislist { set; get; } = new List<string>();
+        int N { set; get; }
+        void DFS(int depth, int left, int right, string p)
+        {
+            if (depth < 2 * N)
+            {
+                if (right <= left && left <= N)
+                {
+                    DFS(depth + 1, left + 1, right, p + "(");
+                    DFS(depth + 1, left, right + 1, p + ")");
+                }
+            }
+            else
+            {
+                if (left == right)
+                {
+                    Parenthesislist.Add(p);
+                }
+            }
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            N = n;
+            DFS(0, 0, 0, "");
+            return Parenthesislist;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Solution()).GenerateParenthesis(3);
+        }
+    }
+}
